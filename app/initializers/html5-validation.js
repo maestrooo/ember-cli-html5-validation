e6e469a276d64d9ff3fa6c7e318c8ebe5668d405
Michaël Gallego <mic.gallego@gmail.com>
2016-03-11 19:54:28 +0100

    Forgot initializers

---
+++ app/initializers/html5-validation.js
@@ -0,0 +1,8 @@
+import 'ember-cli-html5-validation/ext/checkbox';
+import 'ember-cli-html5-validation/ext/text-area';
+import 'ember-cli-html5-validation/ext/text-field';
+
+export default {
+  name: 'ember-cli-html5-validation',
+  initialize: function() {}
+};
